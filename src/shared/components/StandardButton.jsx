c5943405d3c2214d367e6a4ecd4c3147394bdbde
Juye0nLee <dlwndus708@naver.com>
2024-10-29 15:34:52 +0900

    💄 기본 컴포넌트 생성 및 배치

--- src/shared/components/StandardButton.jsx
+++ src/shared/components/StandardButton.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
-
-export default function StandardButton() {
-  return (
-    <div>
-      
-    </div>
-  )
+import styled from 'styled-components'
+export default function StandardButton({ text, width, height,marginRight }) {
+    return (
+        <MainLayout
+            width={width}
+            height={height}
+            marginRight={marginRight}
+        >
+            {text}
+        </MainLayout>
+    )
 }
+
+const MainLayout = styled.button`
+width: ${({ width }) => (width  || "20.375rem")};
+height:${({ height }) => (height || "3.25rem")};
+background-color: #0E0E0F;
+color:  #FFF;
+border: none;
+display : flex;
+justify-content : center;
+align-items : center;
+font-family: Pretendard;
+font-size: 0.875rem;
+font-style: normal;
+font-weight: 600;
+line-height: 133.072%; /* 1.16438rem */
+letter-spacing: 0.03063rem;
+margin-right : ${({ marginRight }) => (marginRight || "0rem")};
+`
