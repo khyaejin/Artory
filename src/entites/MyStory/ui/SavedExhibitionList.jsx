40ae532ce30dbec51af728af6f156d0aa19e9685
JuyeonLee <104489022+Juye0nLee@users.noreply.github.com>
2024-10-31 18:15:16 +0900

    🔧 더미데이터 파일 추가 (#9)

--- src/entites/MyStory/ui/SavedExhibitionList.jsx
+++ src/entites/MyStory/ui/SavedExhibitionList.jsx
@@ -1,13 +1,43 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Exhibitions } from '../../../shared/dummy/ExhibitionDummy';
+
 export default function SavedExhibitionList() {
-  return (
-    <MainLayout>
-      저장한 전시
-    </MainLayout>
-  )
+    return (
+        <MainLayout>
+            <Title>저장한 전시</Title>
+            <ExhibitionContainer>
+                {
+                    Exhibitions.map((e, i) => (
+                        <ExhibitionItem key={i}>
+                            <img src={e.포스터} />
+                        </ExhibitionItem>
+                    ))
+                }
+            </ExhibitionContainer>
+        </MainLayout>
+    )
 }
 
 const MainLayout = styled.div`
-
+width : 70%;
+justify-content : space-evenly;
+margin-top : 8.44rem;
+`;
+const Title = styled.div`
+color: #000;
+font-family: Pretendard;
+font-size: 1.875rem;
+font-style: normal;
+font-weight: 700;
+line-height: 133.072%;
+letter-spacing: 0.06563rem;
+margin-bottom : 1.12rem;
+`
+const ExhibitionContainer = styled.div`
+display: grid;
+grid-template-columns: 1fr 1fr 1fr 1fr;
+`
+const ExhibitionItem = styled.div`
+margin-bottom : 2.94rem;
 `;
