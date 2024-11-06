e56d28f2c4fb1347e72124a7e0bb4dc1eff407aa
binniejung <ybe1027@naver.com>
2024-11-06 14:37:36 +0900

    🎨 StandardPoster 파일, CarouselStory 파일 코드 수정

--- src/shared/components/StandardPoster.jsx
+++ src/shared/components/StandardPoster.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
+import { useLocation, useNavigate } from "react-router-dom";
 
 //이미지
 import LIKE from '../../assets/like.svg'
@@ -7,16 +8,29 @@ import FILLLIKE from '../../assets/filllike.svg'
 import SAVE from '../../assets/save.svg'
 import FILLSAVE from '../../assets/fillsave.svg'
 
-export default function StandardPoster({poster}) {
+export default function StandardPoster({index, poster}) {
     const [isLikeClick, setIsLikeClick] = useState(false)
     const [isSaveClick, setIsSaveClick] = useState(false)
     
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const handlePoster = () => {
+      if(location.pathname === '/story') {
+        navigate(`/story/${index}`);
+      } else if (location.pathname === '/exhibition') {
+        navigate(`/exhibition/${index}`);
+      } else {
+        <></>
+      }
+    }
+
   return (
     <MainLayout>
 
         {/* 포스터 이미지 */}
-        <StyledPoster src={poster}/>
-        <Linear/>
+        <StyledPoster src={poster}  onClick={handlePoster}/>
+        <Linear onClick={handlePoster}/>
 
         {/* 좋아요와 저장 버튼 */}
         <Like src={isLikeClick ? FILLLIKE : LIKE} onClick={()=>{isLikeClick ? setIsLikeClick(false) : setIsLikeClick(true)}}/>
@@ -29,6 +43,7 @@ export default function StandardPoster({poster}) {
 const MainLayout = styled.div`
 height: 268px;
 width: 186px; 
+
 position: relative;
 margin-bottom : 20%;
 `
@@ -47,6 +62,7 @@ display: flex;
 justify-content: center;
 align-items: center;
 z-index: 1;
+
 `;
 
 const Like = styled.img`
@@ -54,6 +70,7 @@ position: absolute;
 bottom: 4%;
 left : 7%;
 z-index: 2;
+cursor: pointer;
 `
 
 const Save = styled.img`
@@ -61,5 +78,6 @@ position: absolute;
 bottom: 4%;
 right : 7%;
 z-index: 2;
+cursor: pointer;
 `
 
