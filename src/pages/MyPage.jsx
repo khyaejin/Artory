3f0d57611af3e037d73d3d2528ccca80e0c58bb8
khyaejin <olivia019182@naver.com>
2024-11-23 23:11:18 +0900

    💄 [Style] MyPage 페이지 - TabButton 구현

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -17,13 +17,33 @@ export default function MyPage() {
 
       {/* 저장한 것들 모아보기 섹션 */}
       <TapContainer>
-        <TabButton onClick={() => setIsButtonClicked(1)}>나의 스토리</TabButton>
-        <TabButton onClick={() => setIsButtonClicked(2)}>나의 앨범</TabButton>
-        <TabButton onClick={() => setIsButtonClicked(3)}>저장 유저</TabButton>
-        <TabButton onClick={() => setIsButtonClicked(4)}>저장 스토리</TabButton>
+        <TabButton
+          isActive={isButtonClicked === 1} // 버튼 클릭 시 색 변경을 위해 상태변수 넘겨주기
+          onClick={() => setIsButtonClicked(1)} // 하단 랜더링을 위한 상태함수
+        >
+          나의 스토리
+        </TabButton>
+        <TabButton
+          isActive={isButtonClicked === 2}
+          onClick={() => setIsButtonClicked(2)}
+        >
+          나의 앨범
+        </TabButton>
+        <TabButton
+          isActive={isButtonClicked === 3}
+          onClick={() => setIsButtonClicked(3)}
+        >
+          저장 유저
+        </TabButton>
+        <TabButton
+          isActive={isButtonClicked === 4}
+          onClick={() => setIsButtonClicked(4)}
+        >
+          저장 스토리
+        </TabButton>
       </TapContainer>
 
-      {/* 상태 변수에 따라 렌더링 */}
+      {/* 상태 변수에 따라 다르게 렌더링 */}
       {isButtonClicked === 1 && <MyStory />}
       {isButtonClicked === 2 && <MyAlbum />}
       {isButtonClicked === 3 && <SavedUser />}
@@ -42,28 +62,32 @@ const MainLayout = styled.div`
 
 const TapContainer = styled.div`
   display: flex;
-  justify-content: center;
-  height: 5%;
+  justify-content: space-between;
+  height: 4%;
   width: calc(100% - 28rem); // 양쪽 14rem 여백을 고려해 너비 계산
   display: flex;
   position: sticky; // 스크롤 시 부모 컨테이너 내에서 고정
   margin: 20px;
-  background-color:blue;
 `;
 
 const TabButton = styled.button`
-  padding: 10px 20px;
-  font-size: 16px;
   cursor: pointer;
-  background-color: #f0f0f0;
   border: 1px solid #ddd;
-  border-radius: 5px;
+  width: 13.625rem;
+  height: 2.1875rem;
+  flex-shrink: 0;
+  // 버튼 누르면 배경 색 변경
+  background-color: ${(props) => (props.isActive ? '#0E0E0F' : '#F4F5F7')};
 
-  &:hover {
-    background-color: #e0e0e0;
-  }
+  // 폰트 관련 css ============================================
+  text-align: center;
+  font-family: Pretendard;
+  font-size: 0.875rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 133.072%; /* 1.16438rem */
+  letter-spacing: 0.03063rem;
+  // 버튼 누르면 폰트 색 변경
+  color: ${(props) => (props.isActive ? '#FFF' : '#5A5C62')};
 
-  &:active {
-    background-color: #d0d0d0;
-  }
 `;
