223e4735a73b12d9b8c8089d3df5ce49bf8c9abf
frombunny <rucy011121@gmail.com>
2024-12-01 10:33:16 +0900

    :recycle: 푸터 삽입

--- src/pages/ExhibitionDetail.jsx
+++ src/pages/ExhibitionDetail.jsx
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import { Exhibitions } from '../shared/dummy/ExhibitionDummy';
 import ExhibitionInfo from '../entites/ExhibitionDetail/ExhibitionInfo';
 import ExhibtionMap from '../entites/ExhibitionDetail/ExhibitionMap';
+import Footer from '../entites/Home/Footer';
 
 export default function ExhibitionDetail() {
   const { id } = useParams(); // useParmas로 URL에서 id 가져오기
@@ -25,7 +26,7 @@ export default function ExhibitionDetail() {
           {/* 전시 장소 지도 컴포넌트 */}
           <ExhibtionMap address={exhibition.주소} place={exhibition.장소}/>
       </ExhibitionLocationContainer>
-
+      <Footer />
     </ExhibitionDetailLayout>
   );
 }
@@ -34,7 +35,7 @@ const ExhibitionDetailLayout = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding : 135px 214px ;
+  padding : 135px 0px 0px 0px ;
 `;
 
 const ExhibitionTopContainer = styled.div`
