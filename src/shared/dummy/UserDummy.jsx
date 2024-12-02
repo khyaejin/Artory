fee5fb10cadaeebfe2b564dc807d3e0daa6df0fd
khyaejin <olivia019182@naver.com>
2024-12-02 14:16:57 +0900

    🚨 MyPage 페이지 - 한줄소개 json으로 가져오도록 수정

--- src/shared/dummy/UserDummy.jsx
+++ src/shared/dummy/UserDummy.jsx
@@ -15,45 +15,63 @@ export const Users = [
         id: 1,
         name: "나야나",
         profile: USER1,
+        oneLineIntroduction : "전시 보러다니는게 유일한 취미일지도",
+        profileTags : "#작가전시위주 #취미 #프로N잡러"
     },
     {
         id: 2,
         name: "일기는 작심삼일",
         profile: USER2,
+        oneLineIntroduction : "전시 좋아 인간",
+        profileTags : "#디자인 #취미 #전시가좋아요"
     },
     {
         id: 3,
         name: "마니또",
         profile: USER3,
+        oneLineIntroduction : "미대생",
+        profileTags : "#미술 #전공 #전시"
     },
     {
         id: 4,
         name: "전시덕후",
         profile: USER4,
+        oneLineIntroduction : "전시 is my life",
+        profileTags : "#음악 #취미 #전시"
     },
     {
         id: 5,
         name: "새벽감성",
         profile: USER5,
+        oneLineIntroduction : "전시는 내 삶의 이유",
+        profileTags : "#고흐 #취미 #프로N잡러"
     },
     {
         id: 6,
         name: "빵덕",
         profile: USER6,
+        oneLineIntroduction : "전시 같이 볼 사람 구해요",
+        profileTags : "#작가전시위주 #취미 #프로N잡러"
     },
     {
         id: 7,
         name: "지나가는 관객",
         profile: USER7,
+        oneLineIntroduction : "I가 전시를 즐기는 방법",
+        profileTags : "#조용하게즐기기 #취미 #MBTI"
     },
     {
         id: 8,
         name: "호기심 많은 여행자",
         profile: USER8,
+        oneLineIntroduction : "J가 전시를 즐기는 방법",
+        profileTags : "#계획적 #취미 #MBTI"
     },
     {
         id: 9,
         name: "성공한덕후",
         profile: USER9,
+        oneLineIntroduction : "공대생의 전시일기",
+        profileTags : "#공대생 #취미 #전시"
     }
 ];
